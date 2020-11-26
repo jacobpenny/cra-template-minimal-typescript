00e871bc900e57bb7ef6b5121bf093d9c53d84b8
Jacob Penny <jacob.penny@gmail.com>
2020-11-26 11:18:49 -0400

    Initial commit

---
+++ template/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.scss';
+
+export function App() {
+  return (
+    <div className="app">
+      <header className="app__header">Minimal Typescript Template</header>
+    </div>
+  );
+}
